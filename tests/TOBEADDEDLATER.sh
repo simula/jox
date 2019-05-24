1185283d9ce16be60c6139f3c71a36117287221d
sakura <arouk@ubuntu16:macbookair>
2019-05-24 08:34:03 +0200

    update to support ubuntu 16

---
+++ tests/TOBEADDEDLATER.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+juju model-config enable-os-refresh-update=false enable-os-upgrade=false
+
+
+sudo rm /etc/systemd/system/jujud*
+sudo rm -rf /var/lib/juju
+sudo rm /usr/bin/juju-run
+
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install tmux curl bridge-utils cloud-image-utils cloud-utils cpu-checker distro-info genisoimage libaio1 libibverbs1 libnl-3-200 libnl-route-3-200 libnspr4 libnss3 librados2 librbd1 msr-tools qemu-block-extra qemu-utils sharutils ubuntu-fan -y
+
+
+
